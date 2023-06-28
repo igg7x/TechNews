4cac97aa8d9467990bc3e0359a2ab96cee574187
Ignacio Gonzalez <ignagonzalez2003@gmail.com>
2023-06-28 10:42:25 -0300

    fix: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,html}"],
   theme: {
     extend: {
+      colors: {
+        text: "#f0f0f0",
+        background: "#0d0d0d",
+        primaryBtn: "#919191",
+        secondaryBtn: "#080808",
+        accent: "#47595c",
+      },
       fontFamily: {
         title: ["Russo One", "sans-serif"],
         text: ["Poppins", "sans-serif"],
